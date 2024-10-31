f6bfb80ae82f73d734147b72cada6ae019506782
Lorena Sambad Lema <lsambadlema@gmail.com>
2024-10-31 16:17:42 +0100

    Terminado Exercise32

--- src/main/java/com/campusdual/classroom/Exercise32.java
+++ src/main/java/com/campusdual/classroom/Exercise32.java
@@ -3,32 +3,47 @@ package com.campusdual.classroom;
 import com.campusdual.util.Utils;
 
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class Exercise32 {
 
     public static void main(String[] args) {
-
+        String inputString = generateStringToSave(null);
+        printToFile(inputString);
     }
 
     public static String generateStringToSave(String string) {
-
+        if (string != null) {
+            return string;
+        } else {
+            return generateUserInputToSave();
+        }
     }
 
-    private static String generateUserInputToSave(){
+    private static String generateUserInputToSave() {
         StringBuilder sb = new StringBuilder();
         System.out.println("Escribe debajo el texto que quieras. Pulsa \"ENTER\" 2 veces seguidas para finalizar:");
         String string;
-        while(!(string = Utils.string()).isEmpty()){
+
+        while (!(string = Utils.string()).isEmpty()) {
             sb.append(string).append(System.lineSeparator());
         }
         return sb.toString();
     }
 
     public static void printToFile(String string) {
+        String filePath = "src/main/resources/data.txt";
 
+        File file = new File(filePath);
+        file.getParentFile().mkdirs();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(string);
+        } catch (IOException e) {
+            System.err.println("Error al escribir en el archivo: " + e.getMessage());
+        }
     }
-
-
 }
+
